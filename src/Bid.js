3c19de1f7d6b2c84b455c02ebd76bc2de8cdf69c
Jordon Peterson <jordonpropm@gmail.com>
2018-09-04 17:38:57 -0600

    I made the bids sort by price from low to high
    I created a copy of the bid array stored in state, used an array.sort method on the bid objects in that array to sort them by price then rendered the array. It worked!

--- src/Bid.js
+++ src/Bid.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import styled from 'styled-components';
+
+
 
 export default class Bid extends Component {
     render() {
         let company = `Company Name: ${this.props.company}`
         console.log('Bid props', this.props)
         return (
+            <Fragment>
             <div className="no-underline ma1">
                 <h1>I'm a Bid!</h1>
                 <h2 className="f3 black-80 fw4 lh-solid">{this.props.company}</h2>
@@ -13,6 +17,7 @@ export default class Bid extends Component {
                 <p className="black-80 fw3">Comments: {this.props.comment}</p>
 
             </div>
+            </Fragment>
         )
     }
 }
--- src/BidContainer.js
+++ src/BidContainer.js
@@ -13,23 +13,24 @@ text-align: center;
  width: 100%;
 background-color: yellow;
 `
-
 class BidContainer extends Component{
     state = {
         bids: [
-            {company:"ABC PLUMBING",comment:"We da best",price:99,id:123},
-            {company:"ABC PLUMBING",comment:"We da best",price:99,id:1234},
-            {company:"ABC PLUMBING",comment:"We da best",price:99,id:1235}
+            {company:"ABC 1",comment:"We da best",price:99,id:123},
+            {company:"ABC 2",comment:"We da best",price:9,id:1234},
+            {company:"ABC 3",comment:"We da best",price:0,id:1235}
 ]
         }
 
     renderBids () {
         console.log('renderBids',this.props)
-        if (this.state.bids.length === 0) {return <FlexRow><p>We are still waiting on bids!</p></FlexRow>}
 
+        if (this.state.bids.length === 0) {return <FlexRow><p>We are still waiting on bids!</p></FlexRow>}
+        const bidsFromState = [...this.state.bids]
+        bidsFromState.sort((a,b) => { return a.price-b.price})
         return(
             <FlexColumn>
-                {this.state.bids.map(bid => (
+                {bidsFromState.map(bid => (
                     <FlexRow><Bid key={bid.id} company={bid.company} comment = {bid.company} price = {bid.price}> </Bid></FlexRow>
                 ))}
             </FlexColumn>
